cfbe82bc61fa965e33c88f200028dee0bf3e532d
Gustavo Scarpin <40478729+Scarpah600@users.noreply.github.com>
2020-09-23 11:39:08 -0300

    ATUALIZADO

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Tmdb from './Tmdb';
 import './App.css';
 import MovieRow from './components/MovieRow';
 import FeactureMovie from './components/FeaturedMovie';
-
+import Header from './components/Header';
 
 export default () => {
 
   const [movieList, setMovieList] = useState([]);
   const [featureData, setfeatureData] = useState(null);
-
+  const [backHeader, setBlackHeader] = useState(false);
   useEffect(() => {
       const loadAll = async() => {
           //Pegando a lista total
@@ -27,16 +27,37 @@ export default () => {
       loadAll();
 
   }, []);
+
+  useEffect(() =>{
+      const scrollListener = () => {
+        if(window.scrollY > 10){
+          setBlackHeader(true);
+        }else{
+          setBlackHeader(false);
+        }
+      }
+      window.addEventListener('scroll',scrollListener);
+      return () =>{
+      window.removeEventListener('scrol',scrollListener);
+      }
+  },[]);
+
   return(
     <div className="page">
+      
+      <Header black={backHeader}/>
       {featureData &&   <FeactureMovie item={featureData} />}
-      <h1>Desenvolvimento do Gustavo Scarpin</h1>
         <section className="lists">
           {movieList.map((item,key) => (
                
               <MovieRow key={key} title={item.title} items={item.items}></MovieRow>
           ))}
         </section>
+        <footer>
+               Feito pelo Gustavo Scarpin<br></br>
+               Direitos de imagem para netflix<br></br>
+               Dados pegos do site Themoviedb.org
+        </footer>
     </div>
   );
 }
--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import './FeaturedMovie.css';
 
+
 export default({item}) => {
+
+    let firsDate = new Date(item.first_air_date);
+    let genres = [];
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
     return (
-        <div>
             <section className="featured" style={{
                     backgroundSize:'cover',
                     backgroundPosition: 'center',
                     backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
             }}>
-                <div>{item.original_name}</div>
+                <div className="featured--vertical">
+                    <div className="featured--horizontal">
+                        <div className="featured--name">{item.original_name}</div>
+                        <div className="featured--info">
+                        <div className="featured--points">
+                            {item.original_vote_average} Pontos</div>
+                                 <div className="featured--year">{firsDate.getFullYear()}</div>
+                             <div className="featured--seasons">{item.number_of_seasons} Temporada
+                                  {item.number_of_seasons !==1 ? 's': ''}</div>
+                        </div>
+                        <div className="featured--description">
+                            {item.overview}
+                        </div>
+                        <div className="featured--bottons">
+                            <a href={`/watch/${item.id}`} className="featured--watchbutton">Assistir</a>
+                            <a href={`/list/add${item.id}`} className="featured--mylistbutton">+Minha Lista</a>
+                        </div>
+                        <div className="featured-genres"><strong>Generos - {genres.join(', ')}</strong></div>
+                </div>
+                </div>
             </section>
-        </div>
-    )
+    );
 }
